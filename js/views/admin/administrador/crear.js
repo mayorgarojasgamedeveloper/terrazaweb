2ee37222280d3489d643f4c1ae862772452ab974
Fernando Mayorga <mayorgarojasgamedeveloper>
2018-05-15 19:47:13 -0500

    Prueba de cors post

--- js/views/admin/administrador/crear.js
+++ js/views/admin/administrador/crear.js
@@ -13,15 +13,25 @@ $('document').ready(function() {
 
     if(status > 0) return 0;
 
-    var usuario = {
+    /*var usuario = {
       username: $('#username').val(),
       password: md5($('#password').val()),
       name: $('#name').val(),
       last_name: $('#last_name').val(),
       email: $('#email').val(),
       phone: $('#phone').val()
+    }*/
+    
+    var usuario = {
+      username: 'fernando',
+      password: '12345678',
+      name: 'Fernando',
+      last_name: 'Mayorga',
+      email: 'fer@gmail.com',
+      phone: '32435757'
     }
 
+    console.log(usuario);
 
     $.post('https://donleonapi.herokuapp.com/user', usuario, function(result){
         console.log(result);
